8f974b26cfe02b8762608c35bb18167e7a9cb545
jaison7slayer <roejusa7@gmail.com>
2023-08-18 15:20:21 -0500

    primer push api technical challenge

---
+++ src/test/java/com/technical/challenge/app/technicalchallenge/TechnicalChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technical.challenge.app.technicalchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
